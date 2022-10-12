6d8b690c7e92aea7fd0610f14482a7d0baaa997e
Yevhenii <eugenykravchenko@gmail.com>
2022-10-12 15:32:44 +0200

    Changed css

--- src/App.tsx
+++ src/App.tsx
@@ -2,35 +2,37 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="app__wrapper">
-      <header className="app__header">
-        <img
-          src="https://cdn-icons-png.flaticon.com/512/235/235361.png"
-          alt="logo"/>
-      </header>
-      <nav className="app__nav">
-        <div>
-          <a href="#">Profile</a>
+    return (
+        <div className="app">
+            <div className="app__wrapper">
+                <header className="app__header">
+                    <img
+                        src="https://cdn-icons-png.flaticon.com/512/235/235361.png"
+                        alt="logo"/>
+                </header>
+                <nav className="app__nav">
+                    <div>
+                        <a href="#">Profile</a>
+                    </div>
+                    <div>
+                        <a href="#">Messages</a>
+                    </div>
+                    <div>
+                        <a href="#">News</a>
+                    </div>
+                    <div>
+                        <a href="#">Music</a>
+                    </div>
+                    <div>
+                        <a href="#">Settings</a>
+                    </div>
+                </nav>
+                <div className="app__content">
+                    <div>Footer</div>
+                </div>
+            </div>
         </div>
-        <div>
-          <a href="#">Messages</a>
-        </div>
-        <div>
-          <a href="#">News</a>
-        </div>
-        <div>
-          <a href="#">Music</a>
-        </div>
-        <div>
-          <a href="#">Settings</a>
-        </div>
-      </nav>
-      <div className="app__content">
-        <div>Footer</div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
