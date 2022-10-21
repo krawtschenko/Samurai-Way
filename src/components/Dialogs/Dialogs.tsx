8c40254c85a1c181d66e2a14590a1e92c6c47d9b
yevheniikravchenko <eugenykravchenko@gmail.com>
2022-10-21 15:29:44 +0200

    Added Props in Dialogs.tsx

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,7 +1,46 @@
+import style from './Dialogs.module.sass'
+import {NavLink} from "react-router-dom";
+import React from "react";
+
+type DialogsItemType = {
+    id: number
+    name: string
+}
+
+type MessagesItemType = {
+    message: string
+}
+
+const DialogsItem: React.FC<DialogsItemType> = (props) => {
+    return (
+        <div className={style.dialog__item}>
+            <NavLink to={'/dialogs/' + props.id}>{props.name}</NavLink>
+        </div>
+    )
+}
+
+const MessagesItem: React.FC<MessagesItemType> = (props) => {
+    return (
+        <div className={style.messages__item}>{props.message}</div>
+    )
+}
+
 export const Dialogs = () => {
     return (
-        <div>
-            Dialogs!
+        <div className={style.dialog}>
+            <div className={style.dialog__items}>
+                <DialogsItem name={"Cho'Gall"} id={1}/>
+                <DialogsItem name={'Varian'} id={2}/>
+                <DialogsItem name={'Rexxar'} id={3}/>
+                <DialogsItem name={'Thrall'} id={4}/>
+                <DialogsItem name={'Garrosh'} id={5}/>
+                <DialogsItem name={'Jaina'} id={6}/>
+            </div>
+            <div className={style.messages}>
+                <MessagesItem message={'Hello'}/>
+                <MessagesItem message={'Wie geht es dir?'}/>
+                <MessagesItem message={'How are u?'}/>
+            </div>
         </div>
     )
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,23 +3,23 @@ import style from "./Navbar.module.sass"
 import {NavLink} from "react-router-dom";
 
 export const Navbar = () => {
-  return (
-    <div className={style.nav}>
-      <div className={style.nav__item}>
-        <NavLink to={"/profile"} className={style.nav__link} activeClassName={style.nav__link_active}>Profile</NavLink>
-      </div>
-      <div className={style.nav__item}>
-        <NavLink to={"/dialogs"} className={style.nav__link} activeClassName={style.nav__link_active}>Messages</NavLink>
-      </div>
-      <div className={style.nav__item}>
-        <NavLink to={"/news"} className={style.nav__link} activeClassName={style.nav__link_active}>News</NavLink>
-      </div>
-      <div className={style.nav__item}>
-        <NavLink to={"/music"} className={style.nav__link} activeClassName={style.nav__link_active}>Music</NavLink>
-      </div>
-      <div className={style.nav__item}>
-        <NavLink to={"/settings"} className={style.nav__link} activeClassName={style.nav__link_active}>Settings</NavLink>
-      </div>
-    </div>
-  )
+    return (
+        <div className={style.nav}>
+            <div className={style.nav__item}>
+                <NavLink to={"/profile"} activeClassName={style.active}>Profile</NavLink>
+            </div>
+            <div className={style.nav__item}>
+                <NavLink to={"/dialogs"} activeClassName={style.active}>Messages</NavLink>
+            </div>
+            <div className={style.nav__item}>
+                <NavLink to={"/news"} activeClassName={style.active}>News</NavLink>
+            </div>
+            <div className={style.nav__item}>
+                <NavLink to={"/music"} activeClassName={style.active}>Music</NavLink>
+            </div>
+            <div className={style.nav__item}>
+                <NavLink to={"/settings"} activeClassName={style.active}>Settings</NavLink>
+            </div>
+        </div>
+    )
 }
