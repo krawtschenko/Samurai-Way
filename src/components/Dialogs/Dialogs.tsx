397c703a3f938fc150d397e31d2b59ee64c58611
yevheniikravchenko <eugenekravchenko.com>
2022-10-23 21:03:36 +0200

    Refactor dialogs and messages with MAP

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,13 +1,15 @@
 import style from './Dialogs.module.sass'
 import {NavLink} from "react-router-dom";
 import React from "react";
+import {v1} from "uuid";
 
 type DialogsItemType = {
-    id: number
+    id: string
     name: string
 }
 
 type MessagesItemType = {
+    id: string
     message: string
 }
 
@@ -25,21 +27,41 @@ const MessagesItem: React.FC<MessagesItemType> = (props) => {
     )
 }
 
+const dialogs: Array<DialogsItemType> = [
+    {id: v1(), name: "Cho'Gall"},
+    {id: v1(), name: "Varian"},
+    {id: v1(), name: "Rexxar"},
+    {id: v1(), name: "Thrall"},
+    {id: v1(), name: "Garrosh"},
+    {id: v1(), name: "Jaina"},
+]
+
+const messages: Array<MessagesItemType> = [
+    {id: v1(), message: 'Du'},
+    {id: v1(), message: 'Du hast'},
+    {id: v1(), message: 'Du hast mich'}
+]
+
+const dialogsElements = dialogs.map(person => {
+    return (
+        <DialogsItem id={person.id} name={person.name}/>
+    )
+})
+
+const messagesElements = messages.map(message => {
+    return (
+        <MessagesItem id={message.id} message={message.message}/>
+    )
+})
+
 export const Dialogs = () => {
     return (
         <div className={style.dialog}>
             <div className={style.dialog__items}>
-                <DialogsItem name={"Cho'Gall"} id={1}/>
-                <DialogsItem name={'Varian'} id={2}/>
-                <DialogsItem name={'Rexxar'} id={3}/>
-                <DialogsItem name={'Thrall'} id={4}/>
-                <DialogsItem name={'Garrosh'} id={5}/>
-                <DialogsItem name={'Jaina'} id={6}/>
+                {dialogsElements}
             </div>
             <div className={style.messages}>
-                <MessagesItem message={'Hello'}/>
-                <MessagesItem message={'Wie geht es dir?'}/>
-                <MessagesItem message={'How are u?'}/>
+                {messagesElements}
             </div>
         </div>
     )
---
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -0,0 +1,12 @@
+import style from './ProfileInfo.module.sass'
+import React from "react";
+
+export const ProfileInfo = () => {
+    return (
+        <div>
+            <img
+                src="https://funart.pro/uploads/posts/2021-04/1618238917_52-p-savanni-i-redkolesya-zhivotnie-zhivotnie-k-57.jpg"
+                alt="background"/>
+        </div>
+    )
+}
