818d573dcb203787efd6d3141d4c5f5c61491df1
yevheniikravchenko <eugenekravchenko.com>
2022-11-27 15:24:33 +0100

    Added presentation component (43)

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,10 @@ import './App.css';
 import {Header} from "./components/Header/Header";
 import {Navbar} from "./components/Navbar/Navbar";
 import {Profile} from "./components/Profile/Profile";
-import {Dialogs} from "./components/Dialogs/Dialogs";
 import {News} from "./components/News/News";
 import {Music} from "./components/Music/Music";
 import {Settings} from "./components/Settings/Settings";
-import {ActionsType, StoreType} from "./redax/reduxStore";
+import {StoreType} from "./redax/reduxStore";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 
 type AppPropsType = {
