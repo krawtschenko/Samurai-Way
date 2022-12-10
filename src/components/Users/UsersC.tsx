6a083e5cb6ac897d20630253d6971cd92e3c442e
yevheniikravchenko <eugenekravchenko.com>
2022-12-10 18:22:30 +0100

    Refactored Users to class component (54)

--- src/components/Users/UsersC.tsx
+++ src/components/Users/UsersC.tsx
@@ -12,9 +12,7 @@ type UsersPropsType = {
 }
 
 export class UsersC extends React.Component<UsersPropsType> {
-    constructor(props: UsersPropsType) {
-        super(props)
-
+    componentDidMount() {
         axios.get("https://social-network.samuraijs.com/api/1.0/users")
             .then(response => {
                 this.props.setUsers(response.data.items)
